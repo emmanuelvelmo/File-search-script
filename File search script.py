e69e1fe88c44bcac5e130b3ed9e548f10807a7b7
Emmanuel V. <51292782+emmanuelvelmo@users.noreply.github.com>
2025-01-01 14:08:03 -0800

    Add files via upload

---
+++ File search script.py
@@ -0,0 +1,39 @@
+import os
+
+def buscar_archivo(directorio, nombre_archivo):
+    rutas_encontradas = []
+    
+    for raiz, directorios, archivos in os.walk(directorio):
+        for archivo in archivos:
+            # Búsqueda parcial sin distinguir mayúsculas/minúsculas
+            if nombre_archivo.lower() in archivo.lower():
+                rutas_encontradas.append(os.path.join(raiz, archivo))
+                
+    return rutas_encontradas
+
+while True:
+    # Solicitar el directorio
+    directorio = input("Enter directory: ")
+    
+    # Verificar si el directorio existe
+    if not os.path.isdir(directorio):
+        print("Directory not found")
+        
+        continue
+    
+    # Solicitar el nombre del archivo (o parte del nombre)
+    nombre_archivo = input("Enter file name: ")
+    
+    print("------------------------------------")
+    
+    # Buscar el archivo en el directorio
+    rutas_encontradas = buscar_archivo(directorio, nombre_archivo)
+    
+    # Mostrar los resultados
+    if rutas_encontradas:
+        for ruta in rutas_encontradas:
+            print(ruta)
+    else:
+        print("File not found")
+    
+    print("------------------------------------\n")
